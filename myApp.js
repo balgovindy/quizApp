7fe50d30e241d3929f2a3796568ae3f2f914260f
Balgovind Yadav <balgovindy@MITR-9TG1HW.local>
2018-08-28 16:45:58 +0530

    updated

--- myApp.js
+++ myApp.js
@@ -12,6 +12,26 @@ const getJSONData = (data) => {
     question.state = 'unattempt';
   })
   createMainPage();
+  shuffle(window.qBank);
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 const createMainPage = () => {
   $('<div>').attr('id', 'activityWrapper').appendTo('body');
